0cf4de8ccf0907dbf8eef4a14a91f014c9c4adc9
heltutan <heltutan@kpmg.com>
2021-07-12 02:12:58 +0200

    Improve category product's styles

--- frontend/src/app/home/category-products/category-products.component.ts
+++ frontend/src/app/home/category-products/category-products.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 /** RxJS */
-import { iif, of, Subscription, Observable, combineLatest, from } from 'rxjs';
-import { debounceTime, map, switchMap, tap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 /** Services */
 import { ProductsService } from 'src/app/services/products.service';
@@ -13,8 +13,8 @@ import { ProductsService } from 'src/app/services/products.service';
     templateUrl: './category-products.component.html',
     styleUrls: ['./category-products.component.scss']
 })
-export class CategoryProductsComponent implements OnInit {
-    private paramsSubs: Subscription;
+export class CategoryProductsComponent {
+    public products$: Observable<Product[]>;
     public products: Product[];
 
     constructor(
@@ -22,25 +22,27 @@ export class CategoryProductsComponent implements OnInit {
         private _router: Router,
         private _products: ProductsService
     ) {
-        this.getUrlParams();
     }
 
     ngOnInit(): void {
+        this.getUrlParams();
     }
+
+    // this.params$.unsubscribe();
 
     private getUrlParams() {
         /** Observable for router params */
-        this.paramsSubs = this._route.params
+        this.products$ = this._route.params
             .pipe(
                 switchMap(params => {
                     console.log('params: ', params);
                     return this._products.getCategoryProducts(params.cat)
                 })
             )
-            .subscribe(res => {
-                console.log('res: ', res);
-                this.products = [...res];
-            })
-    }
 
+        this.products$.subscribe(products => {
+            this.products = [...products]
+            console.log('this.products: ', this.products);
+        })
+    }
 }
